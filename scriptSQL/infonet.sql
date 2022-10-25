c20e12705929a6b9ba672e346ad591a4c58f1a05
nico <nicolasgabrieltomasin@gmail.com>
2022-10-25 00:50:27 -0300

    script agregado

--- scriptSQL/infonet.sql
+++ scriptSQL/infonet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2022 a las 21:26:48
+-- Tiempo de generación: 25-10-2022 a las 05:49:24
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 7.4.29
 
@@ -24,6 +24,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `login_usuario`
+--
+
+CREATE TABLE `login_usuario` (
+  `id` int(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `authCode` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `login_usuario`
+--
+
+INSERT INTO `login_usuario` (`id`, `mail`, `password`, `authCode`) VALUES
+(8, 'nsngt47@gmail.com', '202cb962ac59075b964b07152d234b70', '7aeb08011c41ffc7dae36a034828122a');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `Id` int(11) NOT NULL,
+  `Tipo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`Id`, `Tipo`) VALUES
+(1, 'ADMIN'),
+(2, 'LECTOR'),
+(3, 'ESCRITOR'),
+(4, 'EDITOR');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -33,8 +74,8 @@ CREATE TABLE `usuario` (
   `password` varchar(50) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
-  `tipo` varchar(10) NOT NULL DEFAULT 'Lector',
-  `estado` varchar(10) NOT NULL DEFAULT 'SinAuth',
+  `tipo` varchar(10) NOT NULL DEFAULT '2',
+  `estado` tinyint(1) NOT NULL DEFAULT 0,
   `telefono` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -43,11 +84,17 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `mail`, `password`, `nombre`, `apellido`, `tipo`, `estado`, `telefono`) VALUES
-(3, 'nsngt47@gmail.com', 'asd', 'Nicolas', 'Tomasin', 'Lector', 'SinAuth', NULL);
+(8, 'nsngt47@gmail.com', '202cb962ac59075b964b07152d234b70', 'Nicolas', 'Tomasin', '2', 1, NULL);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indices de la tabla `usuario`
@@ -60,10 +107,16 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
