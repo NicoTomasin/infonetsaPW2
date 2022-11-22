5dfdfcb3a5e4cfa8131cac1b4572696868f95c1a
nico <nicolasgabrieltomasin@gmail.com>
2022-11-22 09:56:37 -0300

    script

--- helpers/Mailer.php
+++ helpers/Mailer.php
@@ -19,8 +19,8 @@ class Mailer
         $this->mail->SMTPOptions = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true ) );//Send using SMTP
         $this->mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $this->mail->SMTPAuth = true;
-        $this->mail->Username = 'infonetsa955@gmail.com';                     //SMTP username
-        $this->mail->Password = 'ifpeimowemlhqcsu';                               //SMTP password
+        $this->mail->Username = 'nsngt47@gmail.com';                     //SMTP username
+        $this->mail->Password = 'scazgzxxfapifflw';                               //SMTP password
         $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;           //Enable implicit TLS encryption
         $this->mail->Port = 587;
     }
